45f7d2292009c58406e667eef59213817bb2658d
Riddhika2910 <2k23.it2310706@gmail.com>
2026-07-11 16:31:07 +0530

    Time: 6 ms (98.86%) | Memory: 60.6 MB (51.67%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        // Remove boundary-connected land
+        for (int i = 0; i < m; i++) {
+            dfs(grid, i, 0);
+            dfs(grid, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(grid, 0, j);
+            dfs(grid, m - 1, j);
+        }
+
+        // Count remaining land
+        int count = 0;
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                if (grid[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs(int[][] grid, int r, int c) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 0) {
+            return;
+        }
+
+        grid[r][c] = 0;
+
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+}
+
+        
+    
